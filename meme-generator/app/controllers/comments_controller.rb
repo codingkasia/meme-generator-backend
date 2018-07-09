e1843b0cbada82542b0863a5409f5ece5f065b4a
jtorre20 <jtorres1989@gmail.com>
2018-07-09 11:18:25 -0400

    test file

--- meme-generator/app/controllers/comments_controller.rb
+++ meme-generator/app/controllers/comments_controller.rb
@@ -1,7 +1,26 @@
 class CommentsController < ApplicationController
-  def index 
-    @comments = Comment.all
+  def index
+    render json: Comment.all
+  end
 
-    render json: @comments
+  def create
+    comment = Comment.create(comment_params)
+    render json: comment
+  end
+
+  def destroy
+    Comment.destroy(params[:id])
+  end
+
+  def update
+    comment = Comment.find(params[:id])
+    comment.update_attributes(comment_params)
+    render json: comment
+  end
+
+  private
+
+  def fruit_params
+    params.require(:comment).permit(:id, :name, :description)
   end
 end
